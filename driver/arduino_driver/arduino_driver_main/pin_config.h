19b8b59fd209365472f11d8c4494a3d43828cc19
Dusan Todorovic <dtodorovic40@gmail.com>
2023-09-06 23:33:28 +0200

    feat: pin config

---
+++ driver/arduino_driver/arduino_driver_main/pin_config.h
@@ -0,0 +1,35 @@
+#ifndef PIN_CONFIG_HEADER
+#define PIN_CONFIG_HEADER
+
+#define FORWARD_DIRECTION 0
+#define BACKWARDS_DIRECTION 1
+#define LEFT_DIRECTION 0
+#define RIGHT_DIRECTION 1
+
+#define SPEED_OF_SOUND 0.04802
+
+#define COUNTER_PERIOD 255
+
+#define LIGHT_SENSOR_TRESHOLD 400
+#define LIGHT_SENSOR A0
+
+#define input1_GPIO 13
+#define input2_GPIO 12
+#define engineIntensity 11
+
+#define input3_GPIO 8
+#define input4_GPIO 9
+#define directionIntensity 10
+
+#define STOP_LIGHTS_GPIO 2
+#define LIGHTS_GPIO 3
+#define LEFT_LIGHT_GPIO 4
+#define RIGHT_LIGHT_GPIO 5
+
+#define ECHO_PIN 7
+#define TRIGGER_PIN 6
+
+#define TX_GPIO 1
+#define RX_GPIO 0
+
+#endif
